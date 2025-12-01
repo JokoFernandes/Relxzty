0b8071a86e11db5ce42adbb4ee0499e76d990bec
Hi :) <winzhipu1@gmail.com>
2025-12-01 13:25:21 +0700

    Update fish-it.lua

--- s/script/fish-it.lua
+++ s/script/fish-it.lua
@@ -303,3 +303,21 @@ firesignal(RETextNotification.OnClientEvent,
         }
     }
 )
+task.wait(3)
+local RETextNotification = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/TextNotification"]
+
+-- This data was received from the server
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 10,
+        Text = "🥵 New Code: Amba Abuse",
+		AdminId= 75974130,
+        Type = "AdminText",
+        AdminName = "Talon",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
