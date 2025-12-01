b226dab696bf09044893519833ce5d4212a991ff
Hi :) <winzhipu1@gmail.com>
2025-12-01 13:12:27 +0700

    Update fish-it.lua

--- s/script/fish-it.lua
+++ s/script/fish-it.lua
@@ -268,3 +268,38 @@ firesignal(Update.OnClientEvent,
 
 end
 task.wait(delay * total + 5)
+local RETextNotification = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/TextNotification"]
+
+-- This data was received from the server
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 4.5,
+        Text = "I will give you guys a free rod if you type ambasing now",
+		AdminId= 75974130,
+        Type = "AdminText",
+        AdminName = "Talon",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
+
+local RETextNotification = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/TextNotification"]
+
+-- This data was received from the server
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 4.5,
+        Text = "Bye Guys hope you enjoy",
+		AdminId= 75974130,
+        Type = "AdminText",
+        AdminName = "Talon",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
