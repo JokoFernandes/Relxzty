e02b15c33783da115dc0e651cf3a9ac901c68120
Hi :) <winzhipu1@gmail.com>
2025-12-05 19:26:33 +0700

    Create animation.lua

---
+++ s/script/animation.lua
@@ -0,0 +1,15 @@
+local ReelIntermission = game:GetService("ReplicatedStorage").Modules.Animations.ReelIntermission
+local ReelStart = game:GetService("ReplicatedStorage").Modules.Animations.ReelStart
+local ReelingIdle = game:GetService("ReplicatedStorage").Modules.Animations.ReelingIdle
+local StartRodCharge = game:GetService("ReplicatedStorage").Modules.Animations.StartRodCharge
+local RodThrow = game:GetService("ReplicatedStorage").Modules.Animations.RodThrow
+local FishCaught = game:GetService("ReplicatedStorage").Modules.Animations.FishCaught
+local EquipIdle = game:GetService("ReplicatedStorage").Modules.Animations.EquipIdle
+
+ReelIntermission.AnimationId = _G.ReelIntermission
+ReelStart.AnimationId = _G.ReelStart
+ReelingIdle.AnimationId = _G.ReelingIdle
+StartRodCharge.AnimationId = _G.StartRodCharge
+RodThrow.AnimationId = _G.RodThrow
+FishCaught.AnimationId = _G.FishCaught
+EquipIdle.AnimationId = _G.EquipIdle
