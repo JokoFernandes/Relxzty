e7764506e908f8614abfa9ad77a117c48a9b67d0
Hi :) <winzhipu1@gmail.com>
2025-12-06 12:10:27 +0700

    Update ambarod.lua

--- s/script/ambarod.lua
+++ s/script/ambarod.lua
@@ -1,31 +1,55 @@
 local rodimg = "rbxassetid://127771159160485"
+
+-- Ambil service Players
 local player = game:GetService("Players").LocalPlayer
+
+-- Ambil PlayerGui
 local playerGui = player:WaitForChild("PlayerGui")
 
-local rodBackpackImg = game:GetService("Players").LocalPlayer.PlayerGui.Backpack.Display.Tile.Inner.Vector.Image
-local rodbackground = game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].BG.Glow.UIGradient.Color
-local nameRodInven = game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].Padded.Top.Label
-local Rarity = game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].Padded.Top.TierLabel 
-local invTextColor = game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].Padded.Top.TierLabel.UIGradient.Color 
-local backpackRodName = game:GetService("Players").LocalPlayer.PlayerGui.Backpack.Display.Tile.Inner.Tags.ItemName
-local nameRodBackpackColor = game:GetService("Players").LocalPlayer.PlayerGui.Backpack.Display.Tile.Inner.Tags.ItemName.UIGradient
+-- Ambil tampilan gambar rod di backpack
+local rodBackpackImg = playerGui.Backpack.Display.Tile.Inner.Vector
 
--- stats
-local rodstat = playerGui:WaitForChild("Inventory"):WaitForChild("Main"):WaitForChild("Content"):WaitForChild("Pages"):WaitForChild("Rods"):WaitForChild("Ghostfinn Rod"):WaitForChild("Padded"):WaitForChild("Bottom")
-local invrod = playerGui:WaitForChild("Inventory"):WaitForChild("Main"):WaitForChild("Content"):WaitForChild("Pages"):WaitForChild("Rods"):WaitForChild("Ghostfinn Rod"):WaitForChild("BG"):WaitForChild("Vector")
-local invrodbg = playerGui:WaitForChild("Inventory"):WaitForChild("Main"):WaitForChild("Content"):WaitForChild("Pages"):WaitForChild("Rods"):WaitForChild("Ghostfinn Rod"):WaitForChild("BG"):WaitForChild("Glow"):WaitForChild("UIGradient")
+-- Ambil warna UIGradient background rod di inventory
+local rodbackground = playerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].BG.Glow.UIGradient
 
+-- Nama rod di inventory
+local nameRodInven = playerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].Padded.Top.Label
+
+-- Label rarity rod
+local Rarity = playerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].Padded.Top.TierLabel
+
+-- UIGradient text rarity
+local invTextColor = Rarity.UIGradient
+
+-- Nama rod dalam backpack
+local backpackRodName = playerGui.Backpack.Display.Tile.Inner.Tags.ItemName
+
+-- Warna nama rod backpack
+local nameRodBackpackColor = backpackRodName.UIGradient
+
+-- Bagian stats rod
+local rodstat = playerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].Padded.Bottom
+
+-- Gambar rod di inventory
+local invrod = playerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].BG.Vector
+
+-- Background glow inventory rod
+local invrodbg = playerGui.Inventory.Main.Content.Pages.Rods["Ghostfinn Rod"].BG.Glow.UIGradient
+
+-- Ambil stat labels
 local luck = rodstat.Luck
 local speed = rodstat.Speed
 local weight = rodstat.Weight
 
--- LOOP STAT
+-- LOOP PERUBAHAN STATS
 task.spawn(function()
 	while true do
-		-- hanya jalan kalau UI inventory terbuka
-		if invrod:IsDescendantOf(playerGui) then
-			invrodbg.Color = invTextColor
+
+		-- UI Inventory harus Visible baru diganti
+		if playerGui.Inventory.Main.Visible == true then
+			invrodbg.Color = invTextColor.Color
 			invrod.Image = rodimg
+
 			luck.Counter.Text = "66666666%"
 			speed.Counter.Text = "7777777%"
 			weight.Counter.Text = "67M kg"
@@ -35,46 +59,49 @@ task.spawn(function()
 	end
 end)
 
--- ========== NOTIF ==========
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local REObtainedNewFishNotification = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/ObtainedNewFishNotification"]
 
-firesignal(REObtainedNewFishNotification.OnClientEvent, 
-    257,
-    {
-        VariantId = "Galaxy",
-        VariantSeed = 1764769101,
-        Weight = 3.2
-    },
-    {
-        CustomDuration = 5,
-        InventoryItem = {
-            Id = 257,
-            Favorited = false,
-            UUID = "d783dbfb-32de-43dd-86e5-b08c1376351a",
-            Metadata = {
-                VariantId = "Galaxy",
-                VariantSeed = 1764769101,
-                Weight = 3.2
-            }
-        },
-        ItemType = "Items",
-        _newlyIndexed = false,
-        Type = "Item",
-        ItemId = 257
-    },
-    false
+-- NOTIFIKASI OBTAIN FISH
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local REObtainedNewFishNotification =
+	ReplicatedStorage.Packages._Index["sleitnick_net@0.2.0"].net["RE/ObtainedNewFishNotification"]
+
+firesignal(REObtainedNewFishNotification.OnClientEvent,
+	257,
+	{
+		VariantId = "Galaxy",
+		VariantSeed = 1764769101,
+		Weight = 3.2
+	},
+	{
+		CustomDuration = 5,
+		InventoryItem = {
+			Id = 257,
+			Favorited = false,
+			UUID = "d783dbfb-32de-43dd-86e5-b08c1376351a",
+			Metadata = {
+				VariantId = "Galaxy",
+				VariantSeed = 1764769101,
+				Weight = 3.2
+			}
+		},
+		ItemType = "Items",
+		_newlyIndexed = false,
+		Type = "Item",
+		ItemId = 257
+	},
+	false
 )
 
--- LOOP PERUBAHAN NOTIF
+
+-- LOOP PERUBAHAN NOTIFIKASI
 task.spawn(function()
 	while true do
 		local notif = playerGui["Text Notifications"].Frame.Tile.TextFrame.Label
 		local notifimg = playerGui["Text Notifications"].Frame.Tile.TextFrame.VectorFrame.Vector
 
-		-- Cek jika notif muncul / aktif
-		if notif:IsDescendantOf(playerGui)
-			and notif.Text:find("Element Rod")
+		-- Cek apakah muncul notif
+		if notif.Text:find("Element Rod")
 			and notifimg.Image:find("99867965187788") then
 
 			notif.Text = "Ambarod"
@@ -84,6 +111,3 @@ task.spawn(function()
 		task.wait(0.1)
 	end
 end)
-
-
-
