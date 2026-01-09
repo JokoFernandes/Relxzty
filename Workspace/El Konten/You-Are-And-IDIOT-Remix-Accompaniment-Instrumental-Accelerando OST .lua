b8256767f7c330a696c0dae9fd0763ba59ceeef5
Hi :) <winzhipu1@gmail.com>
2026-01-10 00:00:56 +0700

    Create You-Are-And-IDIOT-Remix-Accompaniment-Instrumental-Accelerando OST .lua

---
+++ Workspace/El Konten/You-Are-And-IDIOT-Remix-Accompaniment-Instrumental-Accelerando OST .lua
@@ -0,0 +1,35 @@
+local function addsound(assetPath, duration, bol)
+    local s = Instance.new("Sound")
+    s.SoundId = getcustomasset(assetPath)
+    s.Volume = 1
+    s.Looped = bol
+    s.Parent = workspace
+    s:Play()
+
+    -- auto destroy kalau selesai (non-loop)
+    s.Ended:Connect(function()
+        s:Destroy()
+    end)
+
+    -- destroy manual setelah durasi (jalan di thread terpisah)
+    task.spawn(function()
+        task.wait(duration)
+        if s.Parent then
+            s:Destroy()
+        end
+    end)
+
+    return s -- <--- ini di level fungsi, bukan di dalam spawn
+end
+local sduration = (60 * 4) + 38
+-- path, duration
+local intro = addsound("L-sound-package/intro.mp3", 20, false)
+task.wait(8)
+local backsound = addsound("L-sound-package/backsound.mp3", sduration, false)
+local backsound2 = addsound("L-sound-package/backsound2.mp3", sduration, true)
+task.wait(2)
+local hacksound2 = addsound("L-sound-package/hacksound2.mp3", sduration, true)
+task.wait(5)
+local hacksound1 = addsound("L-sound-package/hacksound1.mp3", sduration, true)
+task.wait(3)
+local repeatS = addsound("L-sound-package/repeat.mp3", sduration + 0.8, true)
