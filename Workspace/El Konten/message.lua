3f3d9f300a5c0db5f02f4a816ebbefe552724a80
Hi :) <winzhipu1@gmail.com>
2026-01-09 20:16:39 +0700

    Update message.lua

--- Workspace/El Konten/message.lua
+++ Workspace/El Konten/message.lua
@@ -5,11 +5,101 @@ local RETextNotification = ReplicatedStorage.Packages._Index["sleitnick_net@0.2.
 -- This data was received from the server
 firesignal(RETextNotification.OnClientEvent, 
     {
-        CustomDuration = 4.5,
-        Text = "Get ready 😈",
-		AdminId= 75974130,
+        CustomDuration = 5,
+        Text = "Hello Everyone",
+		AdminId= 8651913431,
         Type = "AdminText",
-        AdminName = "Talon",
+        AdminName = "Tubers93",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
+task.wait(2)
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 5,
+        Text = "Tubers Was Here",
+		AdminId= 8651913431,
+        Type = "AdminText",
+        AdminName = "Tubers93",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
+task.wait(4)
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 10,
+        Text = "This game is so boring",
+		AdminId= 8651913431,
+        Type = "AdminText",
+        AdminName = "Tubers93",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
+task.wait(5)
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 8,
+        Text = "lets make it more fun",
+		AdminId= 8651913431,
+        Type = "AdminText",
+        AdminName = "Tubers93",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
+task.wait(5)
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 8,
+        Text = "Perfect",
+		AdminId= 8651913431,
+        Type = "AdminText",
+        AdminName = "Tubers93",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
+task.wait(2)
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 8,
+        Text = "it's better now.",
+		AdminId= 8651913431,
+        Type = "AdminText",
+        AdminName = "Tubers93",
+        TextColor = {
+            B = 0,
+            G = 0,
+            R = 0
+        }
+    }
+)
+task.wait(2)
+firesignal(RETextNotification.OnClientEvent, 
+    {
+        CustomDuration = 8,
+        Text = "We walk the talk, not only talk the talk",
+		AdminId= 8651913431,
+        Type = "AdminText",
+        AdminName = "Tubers93",
         TextColor = {
             B = 0,
             G = 0,
