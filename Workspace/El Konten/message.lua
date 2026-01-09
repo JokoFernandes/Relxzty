f880ef374c84a9608b0d0164c1d9058c63f32795
Hi :) <winzhipu1@gmail.com>
2026-01-09 20:36:43 +0700

    Update message.lua

--- Workspace/El Konten/message.lua
+++ Workspace/El Konten/message.lua
@@ -32,10 +32,10 @@ firesignal(RETextNotification.OnClientEvent,
         }
     }
 )
-task.wait(4)
+task.wait(4.4)
 firesignal(RETextNotification.OnClientEvent, 
     {
-        CustomDuration = 10,
+        CustomDuration = 8,
         Text = "This game is so boring",
 		AdminId= 8651913431,
         Type = "AdminText",
@@ -63,6 +63,69 @@ firesignal(RETextNotification.OnClientEvent,
     }
 )
 task.wait(5)
+function decal(texture, enum, parent)
+    local decal = Instance.new("Decal")
+    decal.Texture = texture
+    decal.Face = enum
+    decal.Parent = parent
+    return decal
+end
+local function changeMaterial(container, newMaterial)
+    -- Loop semua child di dalam container
+    for _, child in ipairs(container:GetChildren()) do
+        if child:IsA("BasePart") then
+            -- Kalau object adalah Part/MeshPart, ubah material
+            child.Material = newMaterial
+            local res = getcustomasset("base.png") or "rbxassetid://126569944133822"
+            decal(res, Enum.NormalId.Top, child)
+            decal(res, Enum.NormalId.Left, child)
+            decal(res, Enum.NormalId.Right, child)
+            decal(res, Enum.NormalId.Front, child)
+            decal(res, Enum.NormalId.Back, child)
+            decal(res, Enum.NormalId.Bottom, child)
+        elseif child:IsA("Model") or child:IsA("Folder") then
+            -- Kalau object adalah Model/Folder, loop lagi ke dalamnya
+            changeMaterial(child, newMaterial)
+        end
+    end
+end
+local Lighting = game:GetService("Lighting")
+
+function setSky(assetId,SunassetId)
+    -- Hapus Sky lama
+    for _, v in ipairs(Lighting:GetChildren()) do
+        if v:IsA("Sky") then
+            v:Destroy()
+        end
+    end
+
+    -- Buat Sky baru
+    local newSky = Instance.new("Sky")
+    newSky.Name = "LSky"
+
+    -- Pakai assetId untuk semua arah
+    newSky.SkyboxBk = assetId
+    newSky.SkyboxDn = assetId
+    newSky.SkyboxFt = assetId
+    newSky.SkyboxLf = assetId
+    newSky.SkyboxRt = assetId
+    newSky.SkyboxUp = assetId
+    newSky.SunTextureId = SunassetId
+    newSky.MoonTextureId = SunassetId
+    newSky.SunAngularSize = 30
+    newSky.MoonAngularSize = 30
+    -- Masukkan ke Lighting
+    newSky.Parent = Lighting
+end
+
+-- Panggil fungsi dengan asset ID
+local sun = getcustomasset("sun.png")
+local sky = getcustomasset("sky.png")
+-- "rbxassetid://126569944133822"
+setSky(sky,sun)
+-- Panggil fungsi untuk workspace
+changeMaterial(workspace, Enum.Material.Neon) -- contoh: ubah semua jadi Neon
+task.wait(10)
 firesignal(RETextNotification.OnClientEvent, 
     {
         CustomDuration = 8,
@@ -107,7 +170,13 @@ firesignal(RETextNotification.OnClientEvent,
         }
     }
 )
-task.wait(2)
+task.wait(1)
+local Players = game:GetService("Players")
+local TextChatService = game:GetService("TextChatService")
+local msg = "<font color='rgb(255, 215, 105)'>King of Java join the game</font>"
+local channel = TextChatService:WaitForChild("TextChannels"):WaitForChild("RBXGeneral")
+channel:DisplaySystemMessage(msg)
+task.wait(8)
 firesignal(RETextNotification.OnClientEvent, 
     {
         CustomDuration = 8,
