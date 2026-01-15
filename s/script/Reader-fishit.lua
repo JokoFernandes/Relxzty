8c78bf0a475a08c76cebf614c08e3f137011dd73
Hi :) <winzhipu1@gmail.com>
2026-01-15 19:52:27 +0700

    Update Reader-fishit.lua

--- s/script/Reader-fishit.lua
+++ s/script/Reader-fishit.lua
@@ -1,6 +1,6 @@
 local HttpService = game:GetService("HttpService")
 local TextChatService = game:GetService("TextChatService")
-
+local player = game:GetService("Players").LocalPlayer
 local WEBHOOK = "https://discord.com/api/webhooks/1441305375574851635/YV0xu1N8-KCGr1WV9x0RwsWiQD48Kxlg3qKd5C1DvS-K1ejfgKGYNY3NE_zQGcx_Bj8G"
 
 local keywords = getgenv().listToRead
@@ -16,18 +16,61 @@ TextChatService.MessageReceived:Connect(function(msg)
 
     -- bersihkan markup
     local cleanedText = removeMarkup(rawText)
-
     -- Cek keyword
     for _, key in ipairs(keywords) do
+         -- cari pola chance (contoh: "chance 1 in 100M")
+         local chanceMatch = cleanedText:match("with a%s+(.-)%s+chance")
+
+        print("Keyword terdeteksi → " .. matchedWord)
+        if chanceMatch then
+            print("Chance terdeteksi → " .. chanceMatch)
+        end
+        local datawh = {
+            ["content"] = cleanedText,
+            ["embeds"] = {
+                {
+                    ["title"] = "Tittle Embed",
+                    ["description"] = "Deskripsi konten di sini",
+                    ["color"] = 16711680, -- Warna (Decimal) atau hex
+
+                    ["thumbnail"] = {
+                        ["url"] = "https://tr.rbxcdn.com/180DAY-768363145abfc634e1b026bdb214fbef/150/150/Image/Png/noFilter"
+                    }, -- koma di sini
+
+                    ["fields"] = {
+                        {
+                            ["name"] = "Name",
+                            ["value"] = player.DisplayName,
+                            ["inline"] = true
+                        },
+                        {
+                            ["name"] = "Caught",
+                            ["value"] = key,
+                            ["inline"] = true
+                        },
+                        {
+                            ["name"] = "Chance",
+                            ["value"] = chanceMatch,
+                            ["inline"] = true
+                        }
+                    }, -- koma di sini
+
+                    ["footer"] = {
+                        ["text"] = "footer text",
+                        ["icon-url"] = "https://tr.rbxcdn.com/180DAY-768363145abfc634e1b026bdb214fbef/150/150/Image/Png/noFilter"
+                    }, -- koma di sini
+
+                    ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ")
+                }
+            }
+        }
         if cleanedText:lower():find(key:lower()) then
 
             request({
                 Url = WEBHOOK,
                 Method = "POST",
                 Headers = { ["Content-Type"] = "application/json" },
-                Body = HttpService:JSONEncode({
-                    content = "@here " .. cleanedText
-                })
+                Body = HttpService:JSONEncode(datawh)
             })
 
             print("TERKIRIM → " .. cleanedText)
