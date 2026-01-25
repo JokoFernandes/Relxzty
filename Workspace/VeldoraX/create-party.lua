0a4fde7d18f450b42af11cf415331a2d97e56a4f
Hi :) <winzhipu1@gmail.com>
2026-01-25 19:12:54 +0700

    Update create-party.lua

--- Workspace/VeldoraX/create-party.lua
+++ Workspace/VeldoraX/create-party.lua
@@ -17,9 +17,138 @@ local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
 local borderColor = Color3.fromRGB(0, 0, 4) 
 local mainBackground = backgroundColor
+local humanoid = char:WaitForChild("Humanoid")
+local cam = workspace.CurrentCamera
+local UIS = game:GetService("UserInputService")
+local RS = game:GetService("RunService")
+local onMobile = not UIS.KeyboardEnabled
+local keysDown = {}
+local rotating = false
+local freecamEnabled = false
+
+local oldCameraType
+local oldCameraCFrame
+
 screenGui.Parent = gethui()
 screenGui.IgnoreGuiInset = true
 screenGui.ResetOnSpawn = false
+--=============================================================
+--Free Cam
+--=============================================================
+local speed = 5
+local sens = .3
+speed /= 2
+if onMobile then sens *= 2 end
+
+-- FUNCTION TOGGLE FREECAM
+local function toggleFreecam()
+	freecamEnabled = not freecamEnabled
+
+	if freecamEnabled then
+		oldCameraType = cam.CameraType
+		oldCameraCFrame = cam.CFrame
+
+		cam.CameraType = Enum.CameraType.Scriptable
+		cam.CFrame = oldCameraCFrame
+
+		humanoid.WalkSpeed = 0
+		humanoid.JumpPower = 0
+	else
+		cam.CameraType = oldCameraType
+
+		humanoid.WalkSpeed = 16
+		humanoid.JumpPower = 50
+
+		keysDown = {}
+		rotating = false
+		UIS.MouseBehavior = Enum.MouseBehavior.Default
+	end
+end
+
+-- RENDER
+local function renderStepped()
+	if not freecamEnabled then return end
+
+	if rotating then
+		local delta = UIS:GetMouseDelta()
+		local cf = cam.CFrame
+		local yAngle = cf:ToEulerAngles(Enum.RotationOrder.YZX)
+		local newAmount = math.deg(yAngle) + delta.Y
+
+		if newAmount > 65 or newAmount < -65 then
+			if not (yAngle < 0 and delta.Y < 0) and not (yAngle > 0 and delta.Y > 0) then
+				delta = Vector2.new(delta.X, 0)
+			end
+		end
+
+		cf *= CFrame.Angles(-math.rad(delta.Y), 0, 0)
+		cf = CFrame.Angles(0, -math.rad(delta.X), 0) * (cf - cf.Position) + cf.Position
+		cf = CFrame.lookAt(cf.Position, cf.Position + cf.LookVector)
+
+		if delta ~= Vector2.new(0, 0) then
+			cam.CFrame = cam.CFrame:Lerp(cf, sens)
+		end
+
+		UIS.MouseBehavior = Enum.MouseBehavior.LockCurrentPosition
+	else
+		UIS.MouseBehavior = Enum.MouseBehavior.Default
+	end
+
+	if keysDown["Enum.KeyCode.W"] then
+		cam.CFrame *= CFrame.new(0, 0, -speed)
+	end
+	if keysDown["Enum.KeyCode.A"] then
+		cam.CFrame *= CFrame.new(-speed, 0, 0)
+	end
+	if keysDown["Enum.KeyCode.S"] then
+		cam.CFrame *= CFrame.new(0, 0, speed)
+	end
+	if keysDown["Enum.KeyCode.D"] then
+		cam.CFrame *= CFrame.new(speed, 0, 0)
+	end
+end
+
+RS.RenderStepped:Connect(renderStepped)
+
+local validKeys = {
+	"Enum.KeyCode.W",
+	"Enum.KeyCode.A",
+	"Enum.KeyCode.S",
+	"Enum.KeyCode.D"
+}
+
+-- INPUT BEGIN
+UIS.InputBegan:Connect(function(Input, gp)
+	if gp then return end
+
+	if not freecamEnabled then return end
+
+	for _, key in pairs(validKeys) do
+		if key == tostring(Input.KeyCode) then
+			keysDown[key] = true
+		end
+	end
+
+	if Input.UserInputType == Enum.UserInputType.MouseButton2 then
+		rotating = true
+	end
+end)
+
+-- INPUT END
+UIS.InputEnded:Connect(function(Input)
+	if not freecamEnabled then return end
+
+	for key in pairs(keysDown) do
+		if key == tostring(Input.KeyCode) then
+			keysDown[key] = false
+		end
+	end
+
+	if Input.UserInputType == Enum.UserInputType.MouseButton2 then
+		rotating = false
+	end
+end)
+
 --==============================================================
 --script attr
 --==============================================================
@@ -341,6 +470,13 @@ execute.MouseButton1Click:Connect(function()
 	task.spawn(function()
 		loadstring(result)()
 	end)
+end)
+local FreecamTitle = gui("TextLabel","TrackTitle",mainBackground,"Tracker",Tools,10,1)
+FreecamTitle.Size = UDim2.new(1,0,0,20)
+local Freecam = gui("TextLabel","FreeCam",mainBackground,"FreeCam",Tools,10,1)
+Freecam.Size = UDim2.new(1,0,0,20)
+Freecam.MouseButton1Click:Connect(function()
+	toggleFreecam()
 end)
 --============================================================================================================
 -- misc
