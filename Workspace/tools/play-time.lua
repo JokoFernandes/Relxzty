85eea4c34737b05cdc9c45013918a89266407621
Hi :) <winzhipu1@gmail.com>
2026-01-28 18:03:41 +0700

    Update play-time.lua

--- Workspace/tools/play-time.lua
+++ Workspace/tools/play-time.lua
@@ -3,28 +3,45 @@ local gui = player:WaitForChild("PlayerGui")
 
 local timer = Instance.new("ScreenGui")
 timer.Name = "Timer"
-timer.Parent = gethui()
+timer.Parent = gethui() -- ganti gethui() biar pasti muncul di studio
 
-local time = Instance.new("TextLabel") -- benerin ejaan
-time.Parent = timer -- WAJIB biar kelihatan
-time.Size = UDim2.new(0, 50, 0, 25)
-time.Position = UDim2.new(0.13, 0, 0, 50) -- turun 50px
+local time = Instance.new("TextLabel")
+time.Parent = timer
+time.Size = UDim2.new(0, 200, 0, 25)
+time.Position = UDim2.new(0.13, 0, 0, 50)
 time.Text = "Text"
-time.BackgroundTransparency =1
+time.BackgroundTransparency = 1
 time.TextColor3 = Color3.fromRGB(255,255,255)
 time.TextSize = 20
 
+local remind = Instance.new("TextLabel")
+remind.Parent = timer -- ✅ WAJIB biar kelihatan
+remind.Text = ""
+remind.Visible = false
+remind.TextSize = 15
+remind.Size = UDim2.new(0, 400, 0, 25)
+remind.BackgroundTransparency = 1
+remind.TextColor3 = Color3.fromRGB(255,255,255)
+remind.Position = UDim2.new(0.13, 0, 0.50, 0) -- ✅ benerin parameternya
+
 task.spawn(function()
-while true do
-local t = math.floor(game.Workspace.DistributedGameTime)
+	while true do
+		local t = math.floor(workspace.DistributedGameTime)
 
-local jam = math.floor(t / 3600)
-local menit = math.floor((t % 3600) / 60)
-local detik = t % 60
+		local jam = math.floor(t / 3600)
+		local menit = math.floor((t % 3600) / 60)
+		local detik = t % 60
 
-time.Text =jam .. "jam".. menit .. "menit" .. detik .. "detik"
-task.wait(1)
-end
+		time.Text = jam .. " jam " .. menit .. " menit " .. detik .. " detik"
+
+		if jam > 500 then
+			remind.Visible = true
+			remind.Text = "Take a break you've been playing too long"
+		end
+
+		task.wait(1)
+	end
 end)
-task.wait(10)
+
+task.wait(100)
 timer:Destroy()
