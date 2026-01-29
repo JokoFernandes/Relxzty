9dde1708cc8536c2aaa312b3067e40e4f16f369d
Hi :) <winzhipu1@gmail.com>
2026-01-29 15:16:22 +0700

    Update Notif.lua

--- s/script/Notif.lua
+++ s/script/Notif.lua
@@ -1,14 +1,32 @@
 local Player = game:GetService("Players")
 local looping = 0
+if not getgenv().ReaderLoaded then
+getgenv().ReaderLoaded = true
+
 local HttpService = game:GetService("HttpService")
-local del = getgenv().DelaySend or 20
+local del = getgenv().DelaySend or 15
 local delay = del / 2
 local player = game:GetService("Players").LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
 local root = char:WaitForChild("HumanoidRootPart")
-local maxCount = getgenv().MaxCount or 5
+local maxCount = getgenv().MaxCount or 4
 local char = player.Character or player.CharacterAdded:Wait()
 char:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(5637.63574, -904.970215, 15352.915, -0.77629143, 2.27446044e-08, -0.630374193, 1.00332525e-08, 1, 2.3725395e-08, 0.630374193, 1.20931185e-08, -0.77629143)
+task.wait()
+char:WaitForChild("HumanoidRootPart").Anchored = true
+local source = game:HttpGet("https://raw.githubusercontent.com/JokoFernandes/Relxzty/refs/heads/main/s/script/Notif.lua")
+getgenv().MaxCount = getgenv().MaxCount
+getgenv().DelaySend = getgenv().DelaySend
+queue_on_teleport(
+   getgenv().MaxCount = getgenv().MaxCount
+getgenv().DelaySend = getgenv().DelaySend
+   source
+)
+task.spawn(function()
+   task.wait(3)
+   char:WaitForChild("HumanoidRootPart").Anchored = false
+end)
+      
 while true do
 for _, crystal in ipairs(workspace.Islands["Crystal Depths"].Crystals:GetChildren()) do
    if crystal:IsA("MeshPart") and crystal:FindFirstChildWhichIsA("ProximityPrompt", true) then
@@ -71,12 +89,9 @@ for _, crystal in ipairs(workspace.Islands["Crystal Depths"].Crystals:GetChildre
            ts:Teleport(game.PlaceId, p)
         end
         task.wait(delay)
-        local source = game:HttpGet("https://raw.githubusercontent.com/JokoFernandes/Relxzty/refs/heads/main/s/script/Notif.lua")
-        getgenv().MaxCount = getgenv().MaxCount
-        getgenv().DelaySend = getgenv().DelaySend
-        queue_on_teleport(source)
         break
     end
 end
 task.wait(delay)
+end
 end
