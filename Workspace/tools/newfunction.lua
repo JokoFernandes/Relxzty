b5c2c27fbf35d057fd75f86ce802b6ef7063e8f7
Hi :) <winzhipu1@gmail.com>
2026-02-21 08:48:07 +0700

    Update newfunction.lua

--- Workspace/tools/newfunction.lua
+++ Workspace/tools/newfunction.lua
@@ -27,3 +27,47 @@ end
 function JSONEncode(val)
   getgenv().gameNewVar.Http:JSONEncode(val)
 end
+
+-- method 
+
+local ex = {}
+function ex:GetPlayTime()
+  local t = math.floor(game.Workspace.DistributedGameTime)
+	local jam = math.floor(t / 3600)
+  local menit = math.floor((t % 3600) / 60)
+  local detik = t % 60
+  local playtime = {
+    H = jam,
+    M = menit,
+    S = detik
+  }
+  return playtime
+end
+
+function ex:HttpScript(script)
+  loadstring(game:HttpGet(script))()
+end
+
+local liudex = {}
+liudex.__index = liudex
+
+function liudex.new(name,prop)
+	local self = setmetatable({}, liudex)
+	self.Name = name
+  self.Property = prop
+	return self
+end
+
+function liudex:GetName()
+	print(self.Name)
+end
+
+function liudex:GetProperty()
+	print(self.Property)
+end
+
+local a = liudex.new("Jorell")
+local b = liudex.new("Budi","Alok")
+
+print(a:GetName())
+print(b:GetProperty())
